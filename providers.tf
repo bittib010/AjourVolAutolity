57d5e58d1ed9df5ed5b5a8403a6e3a396009a5ef
Adrian K. Skaftun <adrian.skaftun@innofactor.com>
2024-10-22 23:44:52 +0200

    Updated to latest versions

--- providers.tf
+++ providers.tf
@@ -1,25 +1,26 @@
 terraform {
-  required_version = ">= 1.6.6"
+  required_version = ">= 1.9.8"
   #https://github.com/hashicorp/terraform/releases
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.88.0"
+      version = ">= 4.6.0"
       # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
     }
     random = {
       source  = "hashicorp/random"
-      version = ">=3.6.0"
+      version = ">=3.6.3"
       # https://registry.terraform.io/providers/hashicorp/random/latest/docs
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~>2.47.0"
+      version = "~>3.0.2"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  subscription_id = "00000000-0000-0000-0000-000000000000"
 }
